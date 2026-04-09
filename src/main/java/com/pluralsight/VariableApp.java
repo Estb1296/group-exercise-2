e0c2b472eb8a6711405b33720edb529f11a87686
ezrateshale <ezra.teshale12@gmail.com>
2026-04-08 20:40:27 -0500

    feat: Step1 of exercise 2 completed.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,6 +2,16 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args){
-        System.out.println("Question 1");
+        System.out.println("Declare a Variable to store your favorite color");
+        String favoriteColor=" ";
+        System.out.println("Declare a variable to store the year you started this class");
+        String yearYUUStarted="2026";
+        System.out.println("Declare a variable to store your middle initial");
+        char middleInitial='S';
+        System.out.println("Declare a variable to store whether you have pets (yes/no)");
+        boolean havePets=true;
+        System.out.println("Declare a variable with a nice message or saying");
+        String message="I hope you have a blessed day where you may have glimpse of happiness and peace.";
+
     }
 }
