c117ccbe1ae8ecc74e169d8c83d213f1493d8c7a
ezrateshale <ezra.teshale12@gmail.com>
2026-04-09 11:09:46 -0500

    feat: Step 3 completed!

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -2,6 +2,12 @@ package com.pluralsight;
 
 public class Reciept {
     public static void main(String[] args) {
-        System.out.println();
+        String name="Ezra";
+        double price=7.50;
+        System.out.printf(name+" bought a cup of coffee from Starbucks for $%.2f",price);
+        System.out.println("\n");
+        System.out.printf("%s bought a cup of coffee from Starbucks for $%.2f",name,price);
+        System.out.println("\n");
+        System.out.println(name+" bought a cup of coffee from Starbucks for $"+String.format("%.2f",price));
     }
 }
