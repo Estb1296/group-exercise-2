2a7833fbae2cd12d8cc2c23841ebad3fab69faf3
ezrateshale <ezra.teshale12@gmail.com>
2026-04-09 12:51:10 -0500

    Step 3 edits in data types

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -4,16 +4,21 @@ public class VariableApp {
     public static void main(String[] args){
         System.out.println("Step1\n");
         System.out.println("Declare a Variable to store your favorite color");
-        String favoriteColor=" ";
+        String favoriteColor="Blue";
+        System.out.println("My favorite color is "+favoriteColor);
         System.out.println("Declare a variable to store the year you started this class");
-        String yearYUUStarted="2026";
+        short yearYUUStarted=2026;
+        System.out.println("I started year-up in "+yearYUUStarted);
         System.out.println("Declare a variable to store your middle initial");
         char middleInitial='S';
+        System.out.println("My middle initial is "+middleInitial);
         System.out.println("Declare a variable to store whether you have pets (yes/no)");
-        boolean havePets=true;
+        String havePets="yes";
+        System.out.println(havePets+", I do have a dog.");
         System.out.println("Declare a variable with a nice message or saying\n");
         System.out.println("Step2\n");
         String message="I hope you have a blessed day where you may have glimpse of happiness and peace.";
+        System.out.println("My message to you today is: "+message);
         String daysOfTheWeek="Thursday";
         System.out.println("Today is "+daysOfTheWeek);
         double priceOfCoffee=4.99;
