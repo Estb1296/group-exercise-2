6376c8d4fcc4a84ebe0fefd7608e01d8de18c089
ezrateshale <ezra.teshale12@gmail.com>
2026-04-09 10:36:16 -0500

    feat: Step2 of exercise 2 is completed.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args){
+        System.out.println("Step1\n");
         System.out.println("Declare a Variable to store your favorite color");
         String favoriteColor=" ";
         System.out.println("Declare a variable to store the year you started this class");
@@ -10,8 +11,17 @@ public class VariableApp {
         char middleInitial='S';
         System.out.println("Declare a variable to store whether you have pets (yes/no)");
         boolean havePets=true;
-        System.out.println("Declare a variable with a nice message or saying");
+        System.out.println("Declare a variable with a nice message or saying\n");
+        System.out.println("Step2\n");
         String message="I hope you have a blessed day where you may have glimpse of happiness and peace.";
-
+        String daysOfTheWeek="Thursday";
+        System.out.println("Today is "+daysOfTheWeek);
+        double priceOfCoffee=4.99;
+        System.out.println("The price of coffee is $"+priceOfCoffee);
+        char favLetter='E';
+        System.out.println("My favorite letter is "+favLetter);
+        System.out.println("Is it going to rain?(true/false)");
+        boolean stateOfWeather=true;
+        System.out.println("The answer is "+stateOfWeather);
     }
 }
